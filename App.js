b3970b6c7c0bd5f543cf18247d0c19e1050fd9fd
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-04-29 17:30:32 -0400

    feat: add History and MetricCard components

--- App.js
+++ App.js
@@ -4,29 +4,17 @@ import AddEntry from './components/AddEntry';
 import { createStore } from 'redux'
 import { Provider } from 'react-redux'
 import reducer from './reducers'
+import History from './components/History'
 
 export default class App extends React.Component {
-  state = {
-    value: 0
-  }
-
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <AddEntry />
+          <View style={{ height: 20 }} />
+          <History />
         </View>
       </Provider>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    marginLeft: 10,
-    marginRight: 10,
-    alignItems: "stretch",
-    justifyContent: "center"
-  }
-})
