f839962804e18fa59145847c1b1ba1b9f822efc7
Weronika Tomaszewska <veratomaszewska@gmail.com>
2020-04-30 14:38:00 -0400

    feat: add tab navigation on Android and iOS

--- App.js
+++ App.js
@@ -1,18 +1,17 @@
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import AddEntry from './components/AddEntry';
-import { createStore } from 'redux'
-import { Provider } from 'react-redux'
-import reducer from './reducers'
-import History from './components/History'
+import { View } from 'react-native';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducers';
+
+import TabNav from './components/TabNav';
 
 export default class App extends React.Component {
   render() {
     return (
       <Provider store={createStore(reducer)}>
         <View style={{ flex: 1 }}>
-          <View style={{ height: 20 }} />
-          <History />
+          <TabNav />
         </View>
       </Provider>
     );
